4e235c1ab8cfcc921a9a28b6af5bc574bcca883e
Tyler Weeres <tweeres04@gmail.com>
2024-03-29 07:57:56 -0700

    OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
+
+const title = 'BC Ferries Conditions Analytics - Plan your trip stress-free'
+const description =
+	"Historical data and analytics tools for BC Ferries' vehicle deck space capacity."
 
 export const metadata: Metadata = {
-  title: "BC Ferries Conditions",
-  description: "See BC ferries trends",
-};
+	title,
+	description,
+	openGraph: {
+		title,
+		description,
+		images: 'https://bcferries-conditions.tweeres.ca/og-image.jpg',
+	},
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
 }
