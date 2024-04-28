2f58270691f02543d957e3a5cde2cfadb3b52370
Tyler Weeres <tweeres04@gmail.com>
2024-04-28 10:02:05 -0700

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,13 +8,19 @@ const title =
 	'BC Ferries Conditions Analytics - Plan your ferry ride stress-free'
 const description =
 	"Historical data and analytics tools for BC Ferries' vehicle deck space capacity."
+const url = 'https://bcferries-conditions.tweeres.ca'
 
 export const metadata: Metadata = {
 	title,
 	description,
+	alternates: {
+		canonical: url,
+	},
 	openGraph: {
 		title,
 		description,
+		url,
+		type: 'website',
 		images: 'https://bcferries-conditions.tweeres.ca/og-image.jpg',
 	},
 }
