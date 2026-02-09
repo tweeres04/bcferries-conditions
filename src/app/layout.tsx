14c4359f37ac36b095d92f95e24c8d8724a9e2c6
Tyler Weeres <tweeres04@gmail.com>
2026-02-09 05:05:05 -0800

    Add simpleanalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,10 @@ export default function RootLayout({
 				) : null}
 				<Navigation />
 				{children}
+				<script
+					async
+					src="https://scripts.simpleanalyticscdn.com/latest.js"
+				></script>
 			</body>
 		</html>
 	)
