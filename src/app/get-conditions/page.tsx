619ed73f7877a589668d7f1aebb374e43a0a96dd
Tyler Weeres <tweeres04@gmail.com>
2026-02-11 06:44:12 -0800

    Remove get-conditions debug page to prevent build failures
    This debug page was prerendering at build time and scraping BC Ferries routes. When BC Ferries returns 500 errors (like for TSA-DUK/DUK-TSA routes), the build fails. Since this is only a debugging tool, removing it prevents fragile builds.

--- src/app/get-conditions/page.tsx
+++
@@ -1,6 +0,0 @@
-import { getConditions } from '../storeEntries'
-
-export default async function GetConditions() {
-	const conditions = await getConditions()
-	return <pre>{JSON.stringify(conditions, null, 2)}</pre>
-}
